7980b94e60e09aec503af4f08448bcc7fb0cce0d
Mira <szewczyk.mira@gmail.com>
2017-02-16 15:18:57 +0000

    Get credentials from db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class SecretsQueryset(models.QuerySet):
+    def get_backend(self, name):
+        return self.get(name=name)
+
+
+class Secrets(models.Model):
+    name = models.CharField(max_length=20)
+    key = models.TextField()
+    secret = models.TextField()
+    objects = SecretsQueryset.as_manager()
+
+    def get_secrets(self):
+        return self.key, self.secret
--- socialapp/backends/google.py
+++ socialapp/backends/google.py
@@ -1,10 +1,12 @@
 from social_core.backends.google import GoogleOAuth2
-from socialapp.secrets import KEY, SECRET
+from app.models import Secrets
 
 
 class CustomGoogleOAuth2(GoogleOAuth2):
+    DB_NAME = 'google-oauth2'
+
     def get_key_and_secret(self):
         """Return tuple with Consumer Key and Consumer Secret for current
         service provider. Must return (key, secret), order *must* be respected.
         """
-        return KEY, SECRET
+        return Secrets.objects.get_backend(self.DB_NAME).get_secrets()
