18f4ad19dc883b8b3d72d08d4f02f18b7c6cfda9
Mira <szewczyk.mira@gmail.com>
2017-02-16 14:21:14 +0000

    Full circle google login

--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/index.html')
+
+
+def done(request):
+    return render(request, 'app/done.html')
--- socialapp/settings.py
+++ socialapp/settings.py
@@ -137,7 +137,6 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/done/'
 SOCIAL_AUTH_STRATEGY = 'social_django.strategy.DjangoStrategy'
 SOCIAL_AUTH_STORAGE = 'social_django.models.DjangoStorage'
-# SOCIAL_AUTH_STORAGE = 'app.models.CustomDjangoStorage'
 SOCIAL_AUTH_GOOGLE_OAUTH_SCOPE = [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/userinfo.profile'
@@ -152,7 +151,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
-    'common.pipeline.require_email',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.mail.mail_validation',
     'social_core.pipeline.user.create_user',
@@ -162,6 +160,10 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
     'social_core.pipeline.debug.debug'
 )
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
+
 
 try:
     from local_settings import *
